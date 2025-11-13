94ba6cae6e51c745853e9df774c98ddb16a79349
Filipemsousa <filipmenzessousa@gmail.com>
2025-11-13 09:13:59 -0300

    criacao do header e angular

---
+++ projeto_final/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { HeaderComponent } from './componentes/header/header.component';
+import { IndexComponent } from './pages/index/index.component';
+
+export const routes: Routes = [
+    {path:"index",component:IndexComponent},
+];
---
+++ projeto_final/src/app/pages/localidades/localidades.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-localidades',
+  imports: [],
+  templateUrl: './localidades.component.html',
+  styleUrl: './localidades.component.css'
+})
+export class LocalidadesComponent {
+
+}
