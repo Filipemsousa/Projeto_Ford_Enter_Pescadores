acd0ffeca1f12840e0abf20ca9caa0e57285b2d9
Filipemsousa <filipmenzessousa@gmail.com>
2025-11-13 11:30:58 -0300

    inicio do login

--- projeto_final/src/app/app.component.ts
+++ projeto_final/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./componentes/header/header.component";
 import { FooterComponent } from './componentes/footer/footer.component';
+import { LoginComponent } from "./pages/login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent,FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- projeto_final/src/app/app.routes.ts
+++ projeto_final/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { IndexComponent } from './pages/index/index.component';
 import { LocalidadesComponent } from './pages/localidades/localidades.component';
 import { PublicacoesComponent } from './pages/publicacoes/publicacoes.component';
 import { ContatoComponent } from './pages/contato/contato.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 export const routes: Routes = [
@@ -10,5 +11,6 @@ export const routes: Routes = [
     {path:"localidades",component:LocalidadesComponent},
     {path:"publicacoes",component:PublicacoesComponent},
     {path:"contato",component:ContatoComponent},
+    {path:"login",component:LoginComponent},
     {path:"",redirectTo:"index",pathMatch:"full"},
 ];
