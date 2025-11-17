067b0c06c1ce874e2a66b9d5aa920bbacef7d0ce
Filipemsousa <filipmenzessousa@gmail.com>
2025-11-17 20:19:55 -0300

    login

--- projeto_final/src/app/pages/login/login.component.ts
+++ projeto_final/src/app/pages/login/login.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../componentes/header/header.component";
 import { FooterComponent } from "../../componentes/footer/footer.component";
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  // Campos de formulário
+  username: string = '';
+  password: string = '';
+
+  // Mensagem para feedback
+  message: string = '';
+
+  constructor() {}
+
+  login() {
+    // Validação simples
+    if (!this.username || !this.password) {
+      this.message = 'Preencha todos os campos.';
+      return;
+    }
+
+    
+    if (this.username === 'filipe@gmail.com' && this.password === '1234') {
+      this.message = 'Login realizado com sucesso!';
+      // Aqui você pode redirecionar usando Router:
+      // this.router.navigate(['/home']);
+    } else {
+      this.message = 'Usuário ou senha incorretos.';
+    }
+  }
 
 }
